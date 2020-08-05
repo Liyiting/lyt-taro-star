ca948c147eadacaf56460aa8923cb7ce27432628
liyiting <liyiting806@163.com>
2020-08-05 21:36:30 +0800

    upgrade

--- src/app.config.js
+++ src/app.config.js
@@ -1,8 +1,21 @@
 export default {
   pages: [
-    'pages/resume/index',
-    'pages/index/index'
+    'pages/index/index',
+    'pages/resume/index'
   ],
+  tabBar: {
+    list: [{
+      pagePath: 'pages/index/index',
+      text: '首页'
+    }, {
+      pagePath: 'pages/resume/index',
+      text: 'resume'
+    }],
+    'color': '#000',
+    'selectedColor': '#56abe4',
+    'backgroundColor': '#fff',
+    'borderStyle': 'white'
+  },
   window: {
     backgroundTextStyle: 'light',
     navigationBarBackgroundColor: '#fff',
